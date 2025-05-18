f6f426119a6fd56062e854a817d50962550c7424
Leandro-Santana013 <ls1393424@gmail.com>
2025-05-17 23:33:07 -0700

    logica

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double salarioHora;
+int NumeroHora, NumeroHoraEx;
+
+double Calcular(double salarioHora, int NumeroHora, int NumeroHoraEx)
+{
+    return NumeroHora * salarioHora + (NumeroHoraEx * salarioHora * 1.40);
+}
+
+Console.WriteLine("Digite o salário-hora base:");
+salarioHora = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite o numero de horas trabalhadas:");
+NumeroHora = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite o numero de horas extra trabalhadas:");
+NumeroHoraEx = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Seu salário é: {Calcular(salarioHora, NumeroHora, NumeroHoraEx):C2}");
+
